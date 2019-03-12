64fc94d2b960a28e71534965d1f6f5d0523aa967
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2019-03-12 10:38:43 +0100

    Primo commit

---
+++ Generatore/Generatore/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Generatore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nUno = new System.Windows.Forms.Label();
+            this.lbl_lineaFrazione = new System.Windows.Forms.Label();
+            this.lbl_dUno = new System.Windows.Forms.Label();
+            this.lbl_simboloUno = new System.Windows.Forms.Label();
+            this.lbl_nDue = new System.Windows.Forms.Label();
+            this.lbl_simboloDue = new System.Windows.Forms.Label();
+            this.lbl_nTre = new System.Windows.Forms.Label();
+            this.lbl_simboloTre = new System.Windows.Forms.Label();
+            this.lbl_dDue = new System.Windows.Forms.Label();
+            this.lbl_simboloQuattro = new System.Windows.Forms.Label();
+            this.lbl_dTre = new System.Windows.Forms.Label();
+            this.btn_Genera = new System.Windows.Forms.Button();
+            this.pannelloDenominatore = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkFrazione = new System.Windows.Forms.CheckBox();
+            this.lbl_potenzaNUno = new System.Windows.Forms.Label();
+            this.lbl_potenzaNDue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pannelloDenominatore.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_nUno
+            // 
+            this.lbl_nUno.AutoSize = true;
+            this.lbl_nUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nUno.Location = new System.Drawing.Point(13, 51);
+            this.lbl_nUno.Name = "lbl_nUno";
+            this.lbl_nUno.Size = new System.Drawing.Size(44, 31);
+            this.lbl_nUno.TabIndex = 1;
+            this.lbl_nUno.Text = "00";
+            // 
+            // lbl_lineaFrazione
+            // 
+            this.lbl_lineaFrazione.AutoSize = true;
+            this.lbl_lineaFrazione.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_lineaFrazione.Location = new System.Drawing.Point(8, 1);
+            this.lbl_lineaFrazione.Name = "lbl_lineaFrazione";
+            this.lbl_lineaFrazione.Size = new System.Drawing.Size(257, 31);
+            this.lbl_lineaFrazione.TabIndex = 2;
+            this.lbl_lineaFrazione.Text = "---------------------------";
+            // 
+            // lbl_dUno
+            // 
+            this.lbl_dUno.AutoSize = true;
+            this.lbl_dUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dUno.Location = new System.Drawing.Point(8, 32);
+            this.lbl_dUno.Name = "lbl_dUno";
+            this.lbl_dUno.Size = new System.Drawing.Size(44, 31);
+            this.lbl_dUno.TabIndex = 3;
+            this.lbl_dUno.Text = "00";
+            // 
+            // lbl_simboloUno
+            // 
+            this.lbl_simboloUno.AutoSize = true;
+            this.lbl_simboloUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_simboloUno.Location = new System.Drawing.Point(63, 51);
+            this.lbl_simboloUno.Name = "lbl_simboloUno";
+            this.lbl_simboloUno.Size = new System.Drawing.Size(27, 31);
+            this.lbl_simboloUno.TabIndex = 4;
+            this.lbl_simboloUno.Text = "x";
+            // 
+            // lbl_nDue
+            // 
+            this.lbl_nDue.AutoSize = true;
+            this.lbl_nDue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nDue.Location = new System.Drawing.Point(96, 51);
+            this.lbl_nDue.Name = "lbl_nDue";
+            this.lbl_nDue.Size = new System.Drawing.Size(44, 31);
+            this.lbl_nDue.TabIndex = 5;
+            this.lbl_nDue.Text = "00";
+            // 
+            // lbl_simboloDue
+            // 
+            this.lbl_simboloDue.AutoSize = true;
+            this.lbl_simboloDue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_simboloDue.Location = new System.Drawing.Point(146, 51);
+            this.lbl_simboloDue.Name = "lbl_simboloDue";
+            this.lbl_simboloDue.Size = new System.Drawing.Size(27, 31);
+            this.lbl_simboloDue.TabIndex = 6;
+            this.lbl_simboloDue.Text = "x";
+            // 
+            // lbl_nTre
+            // 
+            this.lbl_nTre.AutoSize = true;
+            this.lbl_nTre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nTre.Location = new System.Drawing.Point(179, 51);
+            this.lbl_nTre.Name = "lbl_nTre";
+            this.lbl_nTre.Size = new System.Drawing.Size(44, 31);
+            this.lbl_nTre.TabIndex = 7;
+            this.lbl_nTre.Text = "00";
+            // 
+            // lbl_simboloTre
+            // 
+            this.lbl_simboloTre.AutoSize = true;
+            this.lbl_simboloTre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_simboloTre.Location = new System.Drawing.Point(58, 32);
+            this.lbl_simboloTre.Name = "lbl_simboloTre";
+            this.lbl_simboloTre.Size = new System.Drawing.Size(27, 31);
+            this.lbl_simboloTre.TabIndex = 8;
+            this.lbl_simboloTre.Text = "x";
+            // 
+            // lbl_dDue
+            // 
+            this.lbl_dDue.AutoSize = true;
+            this.lbl_dDue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dDue.Location = new System.Drawing.Point(91, 32);
+            this.lbl_dDue.Name = "lbl_dDue";
+            this.lbl_dDue.Size = new System.Drawing.Size(44, 31);
+            this.lbl_dDue.TabIndex = 9;
+            this.lbl_dDue.Text = "00";
+            // 
+            // lbl_simboloQuattro
+            // 
+            this.lbl_simboloQuattro.AutoSize = true;
+            this.lbl_simboloQuattro.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_simboloQuattro.Location = new System.Drawing.Point(141, 32);
+            this.lbl_simboloQuattro.Name = "lbl_simboloQuattro";
+            this.lbl_simboloQuattro.Size = new System.Drawing.Size(27, 31);
+            this.lbl_simboloQuattro.TabIndex = 10;
+            this.lbl_simboloQuattro.Text = "x";
+            // 
+            // lbl_dTre
+            // 
+            this.lbl_dTre.AutoSize = true;
+            this.lbl_dTre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dTre.Location = new System.Drawing.Point(174, 32);
+            this.lbl_dTre.Name = "lbl_dTre";
+            this.lbl_dTre.Size = new System.Drawing.Size(44, 31);
+            this.lbl_dTre.TabIndex = 11;
+            this.lbl_dTre.Text = "00";
+            // 
+            // btn_Genera
+            // 
+            this.btn_Genera.Location = new System.Drawing.Point(43, 225);
+            this.btn_Genera.Name = "btn_Genera";
+            this.btn_Genera.Size = new System.Drawing.Size(106, 44);
+            this.btn_Genera.TabIndex = 12;
+            this.btn_Genera.Text = "Genera";
+            this.btn_Genera.UseVisualStyleBackColor = true;
+            this.btn_Genera.Click += new System.EventHandler(this.btn_Genera_Click);
+            // 
+            // pannelloDenominatore
+            // 
+            this.pannelloDenominatore.Controls.Add(this.lbl_dUno);
+            this.pannelloDenominatore.Controls.Add(this.lbl_simboloTre);
+            this.pannelloDenominatore.Controls.Add(this.lbl_dTre);
+            this.pannelloDenominatore.Controls.Add(this.lbl_dDue);
+            this.pannelloDenominatore.Controls.Add(this.lbl_simboloQuattro);
+            this.pannelloDenominatore.Controls.Add(this.lbl_lineaFrazione);
+            this.pannelloDenominatore.Location = new System.Drawing.Point(12, 85);
+            this.pannelloDenominatore.Name = "pannelloDenominatore";
+            this.pannelloDenominatore.Size = new System.Drawing.Size(299, 94);
+            this.pannelloDenominatore.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkFrazione);
+            this.panel1.Controls.Add(this.btn_Genera);
+            this.panel1.Location = new System.Drawing.Point(410, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 272);
+            this.panel1.TabIndex = 14;
+            // 
+            // checkFrazione
+            // 
+            this.checkFrazione.AutoSize = true;
+            this.checkFrazione.Location = new System.Drawing.Point(3, 3);
+            this.checkFrazione.Name = "checkFrazione";
+            this.checkFrazione.Size = new System.Drawing.Size(85, 17);
+            this.checkFrazione.TabIndex = 17;
+            this.checkFrazione.Text = "Con frazione";
+            this.checkFrazione.UseVisualStyleBackColor = true;
+            this.checkFrazione.CheckedChanged += new System.EventHandler(this.checkFrazione_CheckedChanged);
+            // 
+            // lbl_potenzaNUno
+            // 
+            this.lbl_potenzaNUno.AutoSize = true;
+            this.lbl_potenzaNUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_potenzaNUno.Location = new System.Drawing.Point(45, 35);
+            this.lbl_potenzaNUno.Name = "lbl_potenzaNUno";
+            this.lbl_potenzaNUno.Size = new System.Drawing.Size(20, 24);
+            this.lbl_potenzaNUno.TabIndex = 15;
+            this.lbl_potenzaNUno.Text = "x";
+            // 
+            // lbl_potenzaNDue
+            // 
+            this.lbl_potenzaNDue.AutoSize = true;
+            this.lbl_potenzaNDue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_potenzaNDue.Location = new System.Drawing.Point(127, 35);
+            this.lbl_potenzaNDue.Name = "lbl_potenzaNDue";
+            this.lbl_potenzaNDue.Size = new System.Drawing.Size(20, 24);
+            this.lbl_potenzaNDue.TabIndex = 16;
+            this.lbl_potenzaNDue.Text = "x";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 335);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_potenzaNDue);
+            this.Controls.Add(this.lbl_potenzaNUno);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pannelloDenominatore);
+            this.Controls.Add(this.lbl_nTre);
+            this.Controls.Add(this.lbl_simboloDue);
+            this.Controls.Add(this.lbl_nDue);
+            this.Controls.Add(this.lbl_simboloUno);
+            this.Controls.Add(this.lbl_nUno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pannelloDenominatore.ResumeLayout(false);
+            this.pannelloDenominatore.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_nUno;
+        private System.Windows.Forms.Label lbl_lineaFrazione;
+        private System.Windows.Forms.Label lbl_dUno;
+        private System.Windows.Forms.Label lbl_simboloUno;
+        private System.Windows.Forms.Label lbl_nDue;
+        private System.Windows.Forms.Label lbl_simboloDue;
+        private System.Windows.Forms.Label lbl_nTre;
+        private System.Windows.Forms.Label lbl_simboloTre;
+        private System.Windows.Forms.Label lbl_dDue;
+        private System.Windows.Forms.Label lbl_simboloQuattro;
+        private System.Windows.Forms.Label lbl_dTre;
+        private System.Windows.Forms.Button btn_Genera;
+        private System.Windows.Forms.Panel pannelloDenominatore;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkFrazione;
+        private System.Windows.Forms.Label lbl_potenzaNUno;
+        private System.Windows.Forms.Label lbl_potenzaNDue;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Generatore/Generatore/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generatore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Genera_Click(object sender, EventArgs e)
+        {
+            if (checkFrazione.Checked == true)
+            {
+                lbl_lineaFrazione.Visible = true;
+                pannelloDenominatore.Visible = true;
+            }
+
+            else {
+                lbl_lineaFrazione.Visible = false;
+                pannelloDenominatore.Visible = false;
+            }
+
+            Random rnd = new Random();
+            Random potenza = new Random();
+            var potenzaTre = potenza.Next(0,2);
+            label1.Text = Convert.ToString(potenzaTre);
+
+            lbl_nUno.Text = Convert.ToString(rnd.Next(1, 99));
+            lbl_nDue.Text = Convert.ToString(rnd.Next(1, 99));
+            lbl_nTre.Text = Convert.ToString(rnd.Next(1, 99));
+            lbl_dUno.Text = Convert.ToString(rnd.Next(1, 99));
+            lbl_dDue.Text = Convert.ToString(rnd.Next(1, 99));
+            lbl_dTre.Text = Convert.ToString(rnd.Next(1, 99));
+
+            if (potenzaTre == 2){
+                lbl_potenzaNUno.Text = "3";}
+            else if (potenzaTre == 1) { lbl_potenzaNUno.Text = "2"; }
+            else { lbl_potenzaNUno.Text = ""; }
+
+            if (lbl_potenzaNUno.Text == "3") { lbl_potenzaNDue.Text = "2"; }
+            else if (lbl_potenzaNUno.Text == "2") { lbl_potenzaNDue.Text = "1"; }
+            else if (lbl_potenzaNUno.Text == "") { lbl_potenzaNDue.Text = ""; }
+
+        }
+
+        private void checkFrazione_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
